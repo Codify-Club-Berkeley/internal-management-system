778e71590b11794049e7a39d6badb9a5e797f149
aidansunbury <aidansunbury@gmail.com>
2023-10-02 18:07:34 -0700

    Finished route

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,10 +1,18 @@
 // /api/user/[id] route
 import { NextResponse, NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export async function GET(
   request: NextRequest,
   { params }: { params: { id: string } },
 ) {
   const { id } = request.id;
-  return NextResponse.json({ id: id }, { status: 200 });
+  const user = prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+  return NextResponse.json(user, { status: 200 });
 }
