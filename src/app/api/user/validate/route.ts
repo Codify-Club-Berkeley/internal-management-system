b462cd6fa36bed913d0209020522688cbacfad76
aidansunbury <aidansunbury@gmail.com>
2023-10-12 15:48:45 -0700

    migrated schema and worked on user creation route

--- src/app/api/user/validate/route.ts
+++ src/app/api/user/validate/route.ts
@@ -5,17 +5,41 @@ import { NextRequest, NextResponse } from "next/server";
 import { redirect } from "next/dist/server/api-utils";
 import { getAuth } from "@clerk/nextjs/server";
 import { clerkClient } from "@clerk/nextjs";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET(req: NextRequest) {
   // Get the current user
-  const user = getAuth(req);
+  const { user } = getAuth(req);
 
-  // Return some information about the user
-  // return NextResponse.json({
-  //   data: user,
-  // });
+  // If not signed in, redirect to sign in page
+  if (!user) {
+    return NextResponse.redirect("/sign-in");
+  }
+
+  // Todo validate that the user has a .berkeley.edu email address
+  // const { emailAddress } = user;
+
+  // Check if the user already exists in our database
+  const existingUser = prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  // If the user does not exist, create the user
+  if (!existingUser) {
+    const createdUser = prisma.user.create({
+      data: {
+        id: user.id,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
   return NextResponse.json({
-    data: user.userId,
+    data: user,
   });
 
   // return NextResponse.redirect(new URL("/roster", req.url).href);
