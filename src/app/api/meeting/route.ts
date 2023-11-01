12faacdb38ce563582a500c4168d0b5de90b83b7
Elaine Shu <elaineshu0755@gmail.com>
2023-11-01 15:09:29 -0700

    began scaffolding for ProjectManager admin-viewable  component

--- src/app/api/meeting/route.ts
+++ src/app/api/meeting/route.ts
@@ -3,6 +3,37 @@ import { PrismaClient, Meeting } from "@prisma/client";
 const prisma = new PrismaClient();
 import { createMeetingValidator } from "@/lib/validators";
 
+/**
+ * @swagger
+ * /api/meeting:
+ *   post:
+ *     description: Create a new meeting
+ *     responses:
+ *       200:
+ *         description: The meeting was successfully created
+ *       400:
+ *         description: There was a problem with the request
+ */
+
+export async function POST(request: NextRequest) {
+  // Get the body of the request
+  const body = await request.json();
+
+  try {
+    createMeetingValidator.parse(body);
+    const meeting = await prisma.meeting.create({
+      data: body,
+    });
+
+    return NextResponse.json(meeting, { status: 200 });
+  } catch (error) {
+    console.log("error");
+    return NextResponse.json(error, {
+      status: 400,
+    });
+  }
+}
+
 export async function POST(request: NextRequest) {
   // Get the body of the request
   const body = await request.json();
