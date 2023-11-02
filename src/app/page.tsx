eded2f19ed41ebadb57e3d4ec717cb333da3462e
Owen Ye <owen2608@berkeley.edu>
2023-11-01 21:06:15 -0700

    Frontend made project team cards start to show up (no image)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,38 @@ import React from "react";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import SignInPrompt from "./sign-in/SignInPrompt";
 import ProjectCard from "./components/ProjectCard";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { User } from "@prisma/client";
 
 export default function Home() {
+  // Because we have multiple queries, we need to rename data and isLoading
+  const { data: currentUser, isLoading: userLoading } = useQuery({
+    queryKey: ["currentUser"],
+    queryFn: async () => {
+      const response = await axios.get("/api/user/me");
+      return response.data;
+    },
+  });
+
   return (
     <div className="container">
       <SignedIn>
         <h1 className="text-2xl font-bold">My Teams</h1>
         <div className=" grid grid-cols-3 gap-5">
-          <ProjectCard
-            teamName="Team 1"
-            imageUrl="/path-to-image.jpg"
-            projectTitle="ims"
-          />
+          {userLoading ? (
+            <h1>loading</h1>
+          ) : (
+            <div>
+              {currentUser.projects.map((project) => (
+                <ProjectCard
+                  teamName="hi"
+                  imageUrl="/path-to-image.jpg"
+                  projectTitle={project.title}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </SignedIn>
       <SignedOut>
