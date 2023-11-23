2f3cbf1e54d44dafa6d14fc0222e76de1b28e2b9
syh379 <103139270+syh379@users.noreply.github.com>
2023-11-23 10:50:43 +0800

    Added Search Bar function on the frontend (#59)
    * using dummy data for project manager component
    
    * changed tags to list of strings
    
    * connect to backend
    
    * resolving type errors in ProjectManager component
    
    * trying to display list of members for each project via querying
    
    * cleaned up react query
    
    * moved some thing around to make it easier in the future
    
    * save changes
    
    * added dropdown functionalities
    
    * try search dropdown
    
    * added searchbar on frontend
    
    * made changes based on reviews
    
    ---------
    
    Co-authored-by: aidansunbury <aidansunbury@gmail.com>
    Co-authored-by: Aidan Sunbury <64103161+aidansunbury@users.noreply.github.com>

---
+++ src/app/components/project-manager/searchbar/SearchResult.tsx
@@ -0,0 +1,34 @@
+import { Checkbox } from "@nextui-org/react";
+import { useState } from "react";
+
+type SearchResultsProps = {
+  result: string;
+  searchResultsChecked: string[];
+  setSearchResultsChecked: (results: Array<string>) => void;
+};
+
+export const SearchResult = ({
+  result,
+  searchResultsChecked,
+  setSearchResultsChecked,
+}: SearchResultsProps) => {
+  const [checked, setChecked] = useState(false);
+  let originalResultsChecked = searchResultsChecked;
+  if (checked) {
+    originalResultsChecked.push(result);
+    setSearchResultsChecked(originalResultsChecked);
+  } else {
+    originalResultsChecked.splice(originalResultsChecked.indexOf(result), 1);
+    setSearchResultsChecked(originalResultsChecked);
+  }
+
+  return (
+    <Checkbox
+      className="m-1"
+      color="warning"
+      onChange={(e) => setChecked(e.target.checked)}
+    >
+      {result}
+    </Checkbox>
+  );
+};
